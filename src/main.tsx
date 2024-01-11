309e0764654ef15ffe26444f40afef19853a336d
bekatru <teamusicband@gmail.com>
2024-01-11 09:14:43 +0600

    Change router file extension

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './index.css';
-import {router} from './modules/routing/router';
+import {router} from './modules/routing/Router';
 import {RouterProvider} from 'react-router-dom';
 
 
