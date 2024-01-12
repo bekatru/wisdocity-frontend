cacac8e63407863be5bfad5f87b10f69e7839ded
bekatru <teamusicband@gmail.com>
2024-01-12 11:21:09 +0600

    Create redux module - Configure store - Create hooks - Install dependencies - Add rule to ESLint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
+import {Provider} from 'react-redux'
+import { store } from './modules/redux'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
 )
