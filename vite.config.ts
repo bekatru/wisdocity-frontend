289d2bf64817dc272b8d43f9b3202e5b64cf9c8d
bekatru <teamusicband@gmail.com>
2024-01-16 13:36:20 +0600

    Add relative route configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      assets: '/src/assets',
+      libs: '/src/libs',
+      modules: '/src/modules',
+      constants: '/src/constants',
+    },
+  },
+});
