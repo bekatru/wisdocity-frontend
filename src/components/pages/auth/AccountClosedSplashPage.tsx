a33353f62d1051bf61dabff2133f2ef2ba72e825
bekatru <teamusicband@gmail.com>
2024-01-17 12:50:28 +0600

    Add more sign up pages

---
+++ src/components/pages/auth/AccountClosedSplashPage.tsx
@@ -0,0 +1,11 @@
+export function AccountClosedSplashPage() {
+    return (
+        <div className="overflow-hidden rounded-lg bg-white shadow max-w-screen-sm">
+            <div className="px-4 py-5 sm:p-6 flex flex-col items-center space-y-8">
+                <h1 className="text-indigo-900 font-semibold text-3xl text-center">
+                    Your accound has been closed successfully
+                </h1>
+            </div>
+        </div>
+    )
+}
--- src/components/pages/auth/index.ts
+++ src/components/pages/auth/index.ts
@@ -2,3 +2,7 @@ export {SignInPage} from './SignInPage';
 export {SignUpPage} from './SignUpPage';
 export {SignUpLearnerPage} from './SignUpLearnerPage';
 export {SignUpExpertPage} from './SignUpExpertPage';
+export {AccountClosedSplashPage} from './AccountClosedSplashPage';
+export {SignUpSplashPage} from './SignUpSplashPage';
+export {UpdateEmailPage} from './UpdateEmailPage';
+export {VerifyEmailPage} from './VerifyEmailPage';
