23f3074de09b8092d31f543fe263ed4067b90155
bekatru <teamusicband@gmail.com>
2024-01-19 12:31:39 +0600

    Install and configure toastify

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import * as ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify';
 import { store } from './modules/redux'
-import { router } from './modules/routing';
+import { router } from './modules/routing'; 
+
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css'
 
 const queryClient = new QueryClient()
@@ -15,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <RouterProvider router={router} />
+        <ToastContainer/>
       </Provider>
     </QueryClientProvider>
   </React.StrictMode>,
