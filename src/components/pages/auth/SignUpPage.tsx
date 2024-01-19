daccaefef27b872dd06121aad409aa7cad9ed5ec
bekatru <teamusicband@gmail.com>
2024-01-19 20:34:31 +0600

    Remove expert word

--- src/components/pages/auth/SignUpPage.tsx
+++ src/components/pages/auth/SignUpPage.tsx
@@ -38,7 +38,7 @@ export function SignUpPage() {
 
   return (
     <AuthPageLayout
-      headerText={`Create ${role === "learner" ? "a" : "an"} ${role} expert account`}
+      headerText={`Create ${role === "learner" ? "a" : "an"} ${role} account`}
       footerText="Already have an account?"
       footerCtaText="Sign in"
       onFooterCtaClick={navigateToSignInPage}
