6fb78944201a5ce589d358749b0638a52431be0e
bekatru <teamusicband@gmail.com>
2024-01-24 12:07:00 +0600

    Delete learner module

--- src/modules/learner/hooks/useCreateLearner/createLearner.request.ts
+++
@@ -1,8 +0,0 @@
-import { api } from "libs/axios";
-import { CreateLearnerPayload, CreateLearnerResponse } from "./createLearner.types";
-
-export const requestCreateLearner = async (
-    body: CreateLearnerPayload
-): Promise<CreateLearnerResponse> => {
-    return await api.post('learner/create', body)
-}
--- src/modules/learner/hooks/useCreateLearner/createLearner.types.ts
+++
@@ -1,8 +0,0 @@
-export type CreateLearnerResponse = unknown
-
-export interface CreateLearnerPayload {
-    purpose: string;
-    topics: string[];
-    way_for_learning: string[],
-    goals: string[],
-}
