51257c651125be2a3672375785ca4e74aa237154
bekatru <teamusicband@gmail.com>
2024-01-30 12:02:50 +0600

    Configure .env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const APP_NAME = "WISDOCITY";
+export const APP_NAME = import.meta.env.VITE_APP_NAME;
 
 export const COOKIE_ACCESS_TOKEN_NAME = `${APP_NAME}-access-token`;
 export const COOKIE_REFRESH_TOKEN_NAME = `${APP_NAME}-refresh-token`;
