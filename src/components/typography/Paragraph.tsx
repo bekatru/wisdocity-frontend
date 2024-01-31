0f35cfc6f131e281c3d01f52c9f7c56ac8ae2069
bekatru <teamusicband@gmail.com>
2024-01-31 11:48:18 +0600

    Update Paragraph component

--- src/components/typography/Paragraph.tsx
+++ src/components/typography/Paragraph.tsx
@@ -1,9 +1,8 @@
+import classNames from "classnames";
 import { PropsWithChildren } from "react";
 
-export function Paragraph(props: PropsWithChildren) {
+export function Paragraph({className, ...props}: React.HTMLAttributes<HTMLParagraphElement> & PropsWithChildren) {
     return (
-        <p className="text-sm leading-5 text-gray-500">
-            {props.children}
-        </p>
+        <p className={classNames("text-sm leading-5 text-gray-500", className)} {...props}/>
     )
 }
