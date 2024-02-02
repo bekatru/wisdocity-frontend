b81bbc2baebdeaaa1a59165a97772f8ea62521df
bekatru <teamusicband@gmail.com>
2024-02-02 21:45:40 +0600

    Change sidebar color

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -116,7 +116,7 @@ export function AppLayout(props: PropsWithChildren) {
                                         </div>
                                     </Transition.Child>
                                     {/* Sidebar component, swap this element with another sidebar if you like */}
-                                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-purple-600 px-6 pb-4">
+                                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-[#64108F] px-6 pb-4">
                                         <div className="flex h-16 shrink-0 items-center">
                                             {/* Logo or company name */}
                                         </div>
@@ -194,7 +194,7 @@ export function AppLayout(props: PropsWithChildren) {
                 {/* Static sidebar for desktop */}
                 <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col">
                     {/* Sidebar component, swap this element with another sidebar if you like */}
-                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-purple-600 px-6 pb-4">
+                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-[#64108F] px-6 pb-4">
                         <div className="flex h-16 shrink-0 items-center justify-center">
                             <div
                                 onClick={() => navigate(Routes.Root)}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "https://api.wisdocity.ai";
 export const ASSISTANT_AI_API_BASE_URL = "https://assistantai.dbsync.io/";
 
 export const APP_NAME = "wisdocity";
