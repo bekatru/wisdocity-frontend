840ee0735bcafd4b167852b9e902f930ef81934c
bekatru <teamusicband@gmail.com>
2024-02-05 20:47:27 +0600

    fix env variables

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.wisdocity.ai";
+export const BASE_URL = "https://api.wisdocity.ai/";
 export const ASSISTANT_AI_API_BASE_URL = "https://assistantai.dbsync.io/";
 
 export const APP_NAME = "wisdocity";
