676c2944c288ed8b325d9178a96288b1bc56b4c8
Sana Javaid <>
2024-02-05 22:15:01 +0500

    build added

--- src/components/pages/expert/profile/Profile.tsx
+++ src/components/pages/expert/profile/Profile.tsx
@@ -1,23 +1,20 @@
-import React, { useState } from 'react';
+
+import { useState } from 'react';
 import { Input, Label } from "components";
 import ProfileIcon from "../../../../assets/profile/profile-user.png";
 import EditIcon from "../../../../assets/profile/icon-edit.svg";
-// import StarRating from 'react-rating-stars-component';
-import StarRating from 'react-rating-stars-component';
 
 export function Profile() {
-    // Sample rating value, replace with your actual rating value
-    const ratingValue = 4.8;
 
     // State variable to manage the open/close state of each accordion
     const [accordionState, setAccordionState] = useState([false, false, false, false]);
 
     // Function to toggle the open/close state of a specific accordion
-    const toggleAccordion = (index) => {
+    const toggleAccordion = (index: number) => {
         const newState = [...accordionState];
         newState[index] = !newState[index];
         setAccordionState(newState);
-    };
+      };
 
     return (
         <>
@@ -36,14 +33,31 @@ export function Profile() {
                                     <div className="text-[24px] fn fw-sb text-[#321841] mb-0">John Wilson</div>
                                     <p className="text-[14px] fn fw-m text-[#321841] mb-[5px]">Investment Expert</p>
                                     <div className="flex items-center">
-                                        <StarRating
-                                            count={5}
-                                            value={ratingValue}
-                                            size={24}
-                                            activeColor="#64108F"
-                                            edit={false}
-                                        />
-                                        <span className="ms-[8px] text-[14px] fn fw-m text-[#6B6985]">{ratingValue} (20 reviews)</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#8726b9" className="w-5 h-5">
+  <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+</svg>
+
+                      
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#8726b9" className="w-5 h-5">
+  <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+</svg>
+
+                      
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#8726b9" className="w-5 h-5">
+  <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+</svg>
+
+                      
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#8726b9" className="w-5 h-5">
+  <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+</svg>
+
+                      
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#8726b9" className="w-5 h-5">
+  <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+</svg>
+
+                                        <span className="ms-[8px] mt-[3px] text-[14px] fn fw-m text-[#6B6985]">4.8 (20 reviews)</span>
                                     </div>
                                 </div>
                                 <div className="ms-auto">
@@ -204,9 +218,9 @@ export function Profile() {
                                     </p>
 
                                     <form>
-                                        <div className="relative">
-                                            <input type="search" id="search" className="h-12 w-[85%] border border-gray-300 rounded-lg bg-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Interviews" required />
-                                            <button type="submit" className="text-white bg-btn-theme fn fw-sb rounded-full px-8 py-3 text-center text-[16px] ml-[10px]">Save</button>
+                                        <div className="flex">
+                                            <input type="search" id="search" className="h-12 min-w-[82%] border border-gray-300 rounded-lg bg-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Interviews" required />
+                                            <button type="submit" className="text-white bg-btn-theme fn fw-sb rounded-full px-8 py-3 text-center text-[16px] ml-[10px] float-right">Save</button>
                                         </div>
                                     </form>
 
