eb32aae7bd5be639f9ab0bd5d620d44f5b5293d6
haroon-j <haroonanwar429@gmail.com>
2024-02-07 21:40:10 +0500

    profile page changes

--- src/components/pages/expert/home/ExpertDashboard.tsx
+++ src/components/pages/expert/home/ExpertDashboard.tsx
@@ -1,34 +1,9 @@
-import { useState } from 'react';
 import { DashboardAvatar } from './DashboardTab';
 
-
 export function ExpertDashboard() {
-    const [activeTab, setActiveTab] = useState('dashboard');
-
-    const handleTabClick = (tabName: string) => {
-        setActiveTab(tabName);
-    };
-
     return (
-        <div className="">
-            <div className="flex space-x-4 flex items-center space-x-8 fn fw-sb text-[16px] text-[#6B6985] mb-[28px] profile-tabs">
-                <button
-                    className={`cursor-pointer px-4 py-3 ${activeTab === 'dashboard'
-                            ? 'current border-b fw-b border-[#321841] text-[#321841]'
-                            : ''
-                        }`}
-                    onClick={() => handleTabClick('dashboard')}
-                >
-                    Dashboard
-                </button>
-            </div>
-
-            {activeTab === 'dashboard' && <DashboardTab />}
-        </div>
+        <DashboardAvatar/>
     );
 }
 
-const DashboardTab = () => <div>
-    <DashboardAvatar/>
-</div>;
 
